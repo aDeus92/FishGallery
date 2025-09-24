66d460a0507404411c280a322fd1afc047aa15ff
aDeus <aleksstroy22@gmail.com>
2025-09-24 11:53:46 +0300

    1

---
+++ app/src/main/java/com/adeus/fishgallery/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.adeus.fishgallery.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFFEEF4F3)
+val OnPrimaryColor = Color(0xFF518DAF)
+val SecondaryColor = Color(0xFFFFFFFF)
+val OnSecondaryColor = Color(0xFF9ECBC6)
+val SurfaceColor = Color(0xFFFFFFFF)
+val OnSurfaceColor = Color(0xFFC8DAC7)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+
+
